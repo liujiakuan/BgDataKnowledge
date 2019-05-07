137a0a3ccd216b0c86ffb57a7e3faf6a35944ff4
liujiakuan <17780574526@163.com>
2019-05-07 22:52:06 +0800

    aggregateByKey operator

--- src/main/scala/operator/O_aggregateByKey.scala
+++ src/main/scala/operator/O_aggregateByKey.scala
@@ -7,7 +7,7 @@ class O_aggregateByKey {
 
 }
 
-object O_aggregateByKey{
+object O_aggregateByKey {
   def apply: O_aggregateByKey = new O_aggregateByKey()
 
   def main(args: Array[String]): Unit = {
@@ -17,5 +17,13 @@ object O_aggregateByKey{
     val rdd2 = spark.sparkContext.parallelize(5 to 20, 3)
     val pairRdd1 = rdd.map(elem => (elem, 1))
     val pairRdd2 = rdd2.map(elem => (elem, 2))
+
+    /**
+      * aggregateByKey
+      * zeroValue 默认值
+      * 先单独各自对各个分区执行function1
+      * 然后各个分区统一合并执行function2
+      */
+    pairRdd1.aggregateByKey(5)(math.max, _ + _).collect().foreach(elem => print(elem + " "))
   }
 }
