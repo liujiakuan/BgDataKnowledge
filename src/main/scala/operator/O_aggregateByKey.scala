00c325c83b53342b1e62ea67b56ebf30f02f0702
liujiakuan <17780574526@163.com>
2019-05-07 22:56:41 +0800

    aggregateByKey operator

--- src/main/scala/operator/O_aggregateByKey.scala
+++ src/main/scala/operator/O_aggregateByKey.scala
@@ -14,9 +14,7 @@ object O_aggregateByKey {
     val conf = new SparkConf().setAppName("aggregateByKey").setMaster("local[2]")
     val spark = SparkSession.builder().config(conf).getOrCreate()
     val rdd = spark.sparkContext.parallelize(1 to 10, 3)
-    val rdd2 = spark.sparkContext.parallelize(5 to 20, 3)
     val pairRdd1 = rdd.map(elem => (elem, 1))
-    val pairRdd2 = rdd2.map(elem => (elem, 2))
 
     /**
       * aggregateByKey
