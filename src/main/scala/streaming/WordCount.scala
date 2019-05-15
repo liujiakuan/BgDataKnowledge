d76d9287c0164bf0d27fcbf7c4c4aa032d2ae04a
liujiakuan <17780574526@163.com>
2019-05-15 22:55:52 +0800

    SparkStreaming  test demo。have some bugs waiting for solving

--- src/main/scala/streaming/WordCount.scala
+++ src/main/scala/streaming/WordCount.scala
@@ -1,8 +1,7 @@
 package streaming
 
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.StreamingContext
-import org.scalatest.time.Seconds
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 class WordCount {
 
@@ -13,11 +12,12 @@ object WordCount{
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("sparkStreaming wordCount").setMaster("local[2]")
-//    val ssc = new StreamingContext(conf,Seconds(2))
-//    val lines = ssc.textFileStream("")
-//    val wordCount = lines.flatMap(_.split(" ")).map(elem => (elem,1)).reduceByKey(_ + _)
-//    wordCount.print()
-//    ssc.start()
-//    ssc.awaitTermination()
+    val ssc = new StreamingContext(conf,Seconds(2))
+    val path = System.getProperty("user.dir")+"\\src\\main\\scala\\streaming\\data.txt"
+    val lines = ssc.textFileStream(path)
+    val wordCount = lines.flatMap(_.split(" ")).map(elem => (elem,1)).reduceByKey(_ + _)
+    wordCount.print()
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
