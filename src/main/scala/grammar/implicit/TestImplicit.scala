9575389bd07fe150a493e45d6885ab52bdd56a91
liujiakuan <17780574526@163.com>
2019-05-31 22:59:20 +0800

    Scala Implicit

--- src/main/scala/grammar/implicit/TestImplicit.scala
+++ src/main/scala/grammar/implicit/TestImplicit.scala
@@ -1,5 +1,14 @@
 package grammar.`implicit`
 
 class TestImplicit {
-
+  /**
+    * 使用方式:
+    * 1.将方法或变量标记为implicit
+    * 2.将方法的参数列表标记为implicit
+    * 3.将类标记为implicit
+    *
+    * Scala支持两种形式的隐式转换：
+    * 隐式值：用于给方法提供参数
+    * 隐式视图：用于类型间转换或使针对某类型的方法能调用成功
+    */
 }
