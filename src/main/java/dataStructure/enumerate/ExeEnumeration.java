55bc4b0e78749b16814f84531a4edc207ea93ba4
liujiakuan <17780574526@163.com>
2019-06-03 21:46:01 +0800

    Enumeration

--- src/main/java/dataStructure/enumerate/ExeEnumeration.java
+++ src/main/java/dataStructure/enumerate/ExeEnumeration.java
@@ -1,7 +1,7 @@
 package dataStructure.enumerate;
 
 public class ExeEnumeration {
-    public static void main(String[]  ljk){
+    public static void main(String[] ljk) {
         System.out.println(Enum.BLUE.getCode());
         System.out.println(Enum.BLUE.getColor());
         System.out.println(Enum.GREEN.getColor());
@@ -9,12 +9,12 @@ public class ExeEnumeration {
         System.out.println(Enum.RED.getColor());
         System.out.println(Enum.RED.getColor());
 
-         Enum[] enums = Enum.values();
-         enums[0].setCode(100);
-         enums[0].setColor("Tomato");
+        Enum[] enums = Enum.values();
+        enums[0].setCode(100);
+        enums[0].setColor("Tomato");
 
-         for(Enum enumsArry:enums){
-             System.out.println(enumsArry);
-         }
+        for (Enum enumsArry : enums) {
+            System.out.println(enumsArry);
+        }
     }
 }
