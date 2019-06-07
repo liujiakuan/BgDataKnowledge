1f270409e817eb599da2a8140e0271886efe3d5f
liujiakuan <17780574526@163.com>
2019-06-07 19:32:35 +0800

    Serializ

---
+++ src/main/java/program/serializable/ExeSerializable.java
@@ -0,0 +1,26 @@
+package program.serializable;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.io.ObjectOutputStream;
+
+public class ExeSerializable {
+    public static void main(String[] ljk) {
+        Employee e = new Employee();
+        e.name = "Reyan Ali";
+        e.address = "Phokka Kuan, Ambehta Peer";
+        e.SSN = 11122333;
+        e.number = 101;
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("D:\\AAAAAAASoft\\IDEA\\BgDataKnowledge\\src\\main\\java\\program\\serializable\\Employee.ser");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(e);
+            objectOutputStream.close();
+            fileOutputStream.close();
+            System.out.print("D:\\AAAAAAASoft\\IDEA\\BgDataKnowledge\\src\\main\\java\\program\\serializable\\Employee.ser");
+        } catch (IOException exeception) {
+            exeception.printStackTrace();
+        }
+    }
+}
