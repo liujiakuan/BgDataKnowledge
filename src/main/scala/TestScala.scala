b809dcaa1251b57e3d3f618b0d94ef2c2cf1050d
liujiakuan <17780574526@163.com>
2019-06-15 15:52:36 +0800

    反编译查看普通对象和伴生对象的区别

--- src/main/scala/TestScala.scala
+++ src/main/scala/TestScala.scala
@@ -4,4 +4,23 @@ class TestScala {
 
 object TestScala {
   def apply: TestScala = new TestScala()
+
+  def main(args: Array[String]): Unit = {
+    TestTwo.func()
+    this.func()
+  }
+
+  def func():Unit={
+    println("testScala")
+  }
+}
+
+object TestOne{
+  def apply: TestScala = new TestScala()
+}
+
+object TestTwo{
+  def func():Unit={
+    println("testtwo")
+  }
 }
