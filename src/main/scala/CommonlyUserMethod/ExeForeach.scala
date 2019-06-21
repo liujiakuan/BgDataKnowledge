c17a41170618e440c18af3210e29e253f0f7f2e6
liujiakuan <17780574526@163.com>
2019-06-21 22:13:30 +0800

    foreach 和 map的区别。foreach迭代指定计算，返回Unit，Map返回指定类型的集合

--- src/main/scala/CommonlyUserMethod/ExeForeach.scala
+++ src/main/scala/CommonlyUserMethod/ExeForeach.scala
@@ -8,7 +8,9 @@ object ExeForeach {
   def main(args: Array[String]): Unit = {
     def apply: ExeForeach = new ExeForeach()
 
+    //foreach 和 map的区别。foreach迭代指定计算，返回Unit，Map返回指定类型的集合
     val seqValue = Seq(1, 2, 3, 4, 5)
     seqValue.foreach(elem => println(elem))
+    seqValue.map(elem => (elem, 1))
   }
 }
